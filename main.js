e558716e7128dca1494cae8cf37f5655e1f14af7
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-03-15 16:28:12 +0900

    update js

--- main.js
+++ main.js
@@ -1,64 +1,77 @@
-const prevBtn = document.querySelector(".slider__btn--left");
-const nextBtn = document.querySelector(".slider__btn--right");
-const sliderImgs = document.querySelectorAll(".slider__img");
-const sliderText = document.querySelectorAll(".slider__text");
+const prevBtn = document.querySelector(".btn-left");
+const nextBtn = document.querySelector(".btn-right");
+const imgDesktop = document.querySelector(".slider__img--desktop");
+const imgMobile = document.querySelector(".slider__img--mobile");
+const title = document.querySelector("[data-title]");
+const desc = document.querySelector("[data-desc]");
 
-//Slider btns//
-// const prevSlide = function () {
-//   const currentImg = document.querySelector(".active-photo");
-//   const currentText = document.querySelector(".active-text");
+const data = [
+  {
+    title: "Discover innovative ways to decorate",
+    desc: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    imgDesktop: "images/desktop-image-hero-1.jpg",
+    imgMobile: "images/mobile-image-hero-1.jpg",
+  },
 
-//   currentImg.classList.remove("active-photo");
-//   currentText.classList.remove("active-text");
+  {
+    title: "We are available all across the globe",
+    desc: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our",
+    imgDesktop: "images/desktop-image-hero-2.jpg",
+    imgMobile: "images/mobile-image-hero-2.jpg",
+  },
 
-//   if (
-//     currentImg.previousElementSibling &&
-//     currentImg.previousElementSibling.classList.contains("slider__img")
-//   ) {
-//     currentImg.previousElementSibling.classList.add("active-photo");
-//     currentText.previousElementSibling.classList.add("active-text");
-//   } else {
-//     sliderImgs[sliderImgs.length - 1].classList.add("active-photo");
-//     sliderText[sliderText.length - 1].classList.add("active-text");
-//   }
-// };
+  {
+    title: "Manufactured with the best materials",
+    desc: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office",
+    imgDesktop: "images/desktop-image-hero-3.jpg",
+    imgMobile: "images/mobile-image-hero-3.jpg",
+  },
+];
 
-// const nextSlide = function () {
-//   const currentImg = document.querySelector(".active-photo");
-//   const currentText = document.querySelector(".active-text");
+//Sliders
+let i = 0; //index of the data
 
-//   currentImg.classList.remove("active-photo");
-//   currentText.classList.remove("active-text");
+nextBtn.addEventListener("click", () => {
+  const end = i === data.length - 1;
+  if (!end) {
+    i++;
+    setData();
+  } else {
+    i = 0;
+    setData();
+  }
+});
 
-//   if (
-//     currentImg.nextElementSibling &&
-//     currentImg.nextElementSibling.classList.contains("slider__img")
-//   ) {
-//     currentImg.nextElementSibling.classList.add("active-photo");
-//     currentText.nextElementSibling.classList.add("active-text");
-//   } else {
-//     sliderImgs[0].classList.add("active-photo");
-//     sliderText[0].classList.add("active-text");
-//   }
-// };
+prevBtn.addEventListener("click", () => {
+  const start = i === 0;
+  if (!start) {
+    i--;
+    setData();
+  } else {
+    i = 0;
+    setData();
+  }
+});
 
-// prevBtn.addEventListener("click", prevSlide);
-// nextBtn.addEventListener("click", nextSlide);
+function setData() {
+  imgDesktop.src = data[i].imgDesktop;
+  imgMobile.src = data[i].imgMobile;
+  title.textContent = data[i].title;
+  desc.textContent = data[i].desc;
+}
 
 //Navigation//
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".close-li");
 const nav = document.querySelector(".header__nav");
-const overlay = document.querySelector("#overlay");
+//const overlay = document.querySelector("#overlay");
 
 const openNav = function () {
   nav.classList.add("open");
-  // overlay.classList.add("dark");
 };
 
 const closeNav = function () {
   nav.classList.remove("open");
-  // overlay.classList.remove("dark");
 };
 
 hamburger.addEventListener("click", openNav);
